0b3e170049c90994f662b8190b4446c8371cddc8
Azavech <azavech@gmail.com>
2025-09-01 17:46:57 -0400

    Update redis for debug redis

--- redis.go
+++ redis.go
@@ -1,11 +1,13 @@
 package service
 
 import (
+	"context"
 	"crypto/tls"
 	"crypto/x509"
 	"fmt"
 
 	"github.com/go-redis/redis/v8"
+	log "github.com/sirupsen/logrus"
 
 	redisDomain "github.com/yeencloud/lib-base/domain/redis"
 	"github.com/yeencloud/lib-shared/config"
@@ -38,6 +40,29 @@ func (bs *BaseService) configureRedis() error {
 		DB:        rdscfg.Database,
 		TLSConfig: tlsConfig,
 	})
+
+	ctx := context.Background()
+	// First try ACL-style AUTH (user+pass) if username non-empty:
+	if rdscfg.Username.Value != "" {
+		if err := redisEngine.Do(ctx, "AUTH", rdscfg.Username.Value, rdscfg.Password.Value).Err(); err != nil {
+			log.Infof("AUTH user+pass failed: %v", err)
+		} else {
+			log.Info("AUTH user+pass succeeded")
+		}
+	}
+
+	// If the above failed, try single-arg AUTH (password-only)
+	if err := redisEngine.Do(ctx, "AUTH", rdscfg.Password.Value).Err(); err != nil {
+		log.Infof("AUTH single-arg failed too: %v", err)
+	} else {
+		log.Info("AUTH single-arg succeeded")
+	}
+
+	// Now Ping
+	if err := redisEngine.Ping(ctx).Err(); err != nil {
+		log.Infof("Ping failed after AUTH: %v", err)
+	}
+	log.Info("connected & authenticated")
 
 	bs.redis = redisEngine
 
