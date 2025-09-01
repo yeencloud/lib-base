d304ab251d94f70a3059b00b00ea9deb71c13768
Azavech <azavech@gmail.com>
2025-09-01 16:37:31 -0400

    Add TLS support for redis

--- domain/redis/redis.go
+++ domain/redis/redis.go
@@ -6,6 +6,8 @@ type RedisConfig struct {
 	// Bind Address
 	Host     string        `config:"REDIS_HOST" default:"localhost"`
 	Port     int           `config:"REDIS_PORT" default:"6379"`
+	Username config.Secret `config:"REDIS_USERNAME" default:"default"`
 	Password config.Secret `config:"REDIS_PASSWORD" default:""`
 	Database int           `config:"REDIS_DATABASE" default:"0"`
+	UseTLS   bool          `config:"REDIS_TLS" default:"false"`
 }
--- redis.go
+++ redis.go
@@ -1,6 +1,8 @@
 package service
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
 
 	"github.com/go-redis/redis/v8"
@@ -15,10 +17,26 @@ func (bs *BaseService) configureRedis() error {
 		return err
 	}
 
+	var tlsConfig *tls.Config = nil
+
+	if rdscfg.UseTLS {
+		certPool, err := x509.SystemCertPool()
+		if err != nil {
+			certPool = x509.NewCertPool()
+		}
+
+		tlsConfig = &tls.Config{
+			MinVersion: tls.VersionTLS12,
+			RootCAs:    certPool,
+		}
+	}
+
 	redisEngine := redis.NewClient(&redis.Options{
-		Addr:     fmt.Sprintf("%s:%d", rdscfg.Host, rdscfg.Port),
-		Password: rdscfg.Password.Value,
-		DB:       rdscfg.Database,
+		Addr:      fmt.Sprintf("%s:%d", rdscfg.Host, rdscfg.Port),
+		Username:  rdscfg.Username.Value,
+		Password:  rdscfg.Password.Value,
+		DB:        rdscfg.Database,
+		TLSConfig: tlsConfig,
 	})
 
 	bs.redis = redisEngine
